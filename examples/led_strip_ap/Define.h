1b0d836e1f5285d043962b8a27fc9f1cd58ac341
MrGh0sT <MrGh0sT@DevMachine.fr>
2019-02-13 19:57:45 +0100

    Add led_strip_ap example for wifi AP LED STRIP

---
+++ examples/led_strip_ap/Define.h
@@ -0,0 +1,7 @@
+#define LED_COUNT 8            // this is the number of WS2812B leds on the strip
+#define SERIAL_NUMBER "APFIRSTLEDSTRIP"
+#define PASSWORD "111-11-111"
+#define FIRMWARE_REVISION "0.2"
+#define MODEL "Generic - LED Strip"
+#define MANUFACTURER "MrGh0sT"
+

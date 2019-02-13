9cb074b359e1481ad5fcc4c07c02720938bbc3a8
MrGh0sT <MrGh0sT@DevMachine.fr>
2019-02-13 22:00:46 +0100

    Add led_strip_animation_ap example for wifi AP ANIMATION LED STRIP

---
+++ examples/led_strip_animation_ap/Define.h
@@ -0,0 +1,7 @@
+#define LED_COUNT 8            // this is the number of WS2812B leds on the strip
+#define SERIAL_NUMBER "2NDTESTFORLEDSTRIP"
+#define PASSWORD "111-11-111"
+#define FIRMWARE_REVISION "0.3"
+#define MODEL "Generic"
+#define MANUFACTURER "MrGh0sT"
+
